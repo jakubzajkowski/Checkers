b062640075a1e3c8cdb8dd9004288d28eadd3aa3
jakubzajkowski <jakudjan123@gmail.com>
2025-02-24 22:56:34 +0100

    Initial checkers commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javax.swing.*;
+
+public class App extends JFrame {
+    public App() {
+        setTitle("Checkers");
+        setSize(600, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
