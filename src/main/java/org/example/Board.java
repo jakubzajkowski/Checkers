1a193ac05b1d04284c07007541b8b8b612e16d9f
jakubzajkowski <jakudjan123@gmail.com>
2025-02-26 00:30:48 +0100

    Implement draw pieces method

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -4,22 +4,27 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Board extends JPanel {
-    private Color primaryColor = Color.WHITE;
-    private Color secondaryColor = Color.RED;
-    private int fieldSize = 70;
+    private final Color primaryColor = Color.GRAY;
+    private final Color secondaryColor = Color.RED;
+    private final int fieldSize = 70;
+    private final int pieceSize = 60;
+    private final int boardSize = 8 * fieldSize;
+
+    private int[][] board = new int[8][8];
 
     public Board() {
+
     }
+
     @Override
     public void paintComponent(Graphics g){
         drawBoard(g);
+        drawPieces(g);
     }
 
     public void drawBoard(Graphics g){
-        int boardSize = 8 * fieldSize;
         int windowWidth = getWidth();
         int windowHeight = getHeight();
-
         int marginX = (windowWidth - boardSize) / 2;
         int marginY = (windowHeight - boardSize) / 2;
 
@@ -31,6 +36,27 @@ public class Board extends JPanel {
                     g.setColor(j % 2 == 0 ? secondaryColor : primaryColor);
                 }
                 g.fillRect(i * fieldSize + marginX, j * fieldSize + marginY, fieldSize, fieldSize);
+            }
+        }
+    }
+    public void drawPieces(Graphics g) {
+        int windowWidth = getWidth();
+        int windowHeight = getHeight();
+        int marginX = (windowWidth - boardSize) / 2 + (fieldSize-pieceSize)/2;
+        int marginY = (windowHeight - boardSize) / 2 + (fieldSize-pieceSize)/2;
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                g.setColor(j==1 || j==0 || j==2 ? Color.white : Color.black);
+                if(j==0 || j==6 || j==2) {
+                    if (i % 2 != 0) {
+                        g.fillOval(i * fieldSize + marginX, j * fieldSize + marginY, pieceSize, pieceSize);
+                    }
+                }if(j==1 || j==5 || j==7) {
+                    if (i % 2 == 0) {
+                        g.fillOval(i * fieldSize + marginX, j * fieldSize + marginY, pieceSize, pieceSize);
+                    }
+                }
             }
         }
     }
